53255f87a49cdcb4c424c806994b660260ddc1b5
Tan Jun Rong <tanjunrong@gmail.com>
2019-09-04 13:38:54 +0900

    first commit

---
+++ buildSrc/src/main/java/Secrets.kt
@@ -0,0 +1,10 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+object Secrets {
+    private const val CURRENCY_LAYER_ACCESS_TOKEN = "CURRENCY_LAYER_ACCESS_TOKEN"
+
+    // val currencyLayerAccessToken = apiKeysProperties().getProperty(CURRENCY_LAYER_ACCESS_TOKEN)
+    val currencyLayerAccessToken = "d0bbf06c7ace118e5df7047d2e56ed6f"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,35 @@
+object Versions {
+    val App = App()
+    val Test = Test()
+    val Tool = Tool()
+    val AndroidConfig = AndroidConfig()
+}
+
+class App {
+    val androidX = "1.1.0-rc01"
+    val rxAndroid = "2.1.1"
+    val rxJava = "2.1.13"
+    val material = "1.1.0-alpha09"
+    val constraintLayout = "2.0.0-beta2"
+    val recyclerView = "1.1.0-beta01"
+    val viewModel = "2.0.0"
+}
+
+class Test {
+    val junit = "4.12"
+    val runner = "1.0.2"
+    val espresso = "3.0.2"
+}
+
+class Tool {
+    val kotlin = "1.3.50"
+    val buildGradle = "3.4.2"
+}
+
+class AndroidConfig {
+    val sdkVersion = 28 // target & compile sdk
+    val minSdkVersion = 21
+    val targetSdkVersion = 28
+    val versionCode = 1
+    val versionName = "0.1.0"
+}
