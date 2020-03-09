e7d158692ae2b8b9a0c932c84e70b4b038655932
Tan Jun Rong <tanjunrong@gmail.com>
2020-03-09 15:05:24 +0900

    locate lint-results.xml take 6

--- temp.rb
+++ temp.rb
@@ -2,5 +2,5 @@ puts "----hello from Ruby----"
 puts "args: #{ARGV}"
 
 puts "Dir.pwd: #{Dir.pwd}"
-file = File.open("./SimpleCurrency/app/build/reports/lint-results.xml")
+file = File.open("./app/build/reports/lint-results.xml")
 puts file.read
